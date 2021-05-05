13d114afb113898a2898957feab4236723035fb7
João Alves <njoao97710@gmail.com>
2021-05-05 06:02:37 +0000

    :sparkles: FEAT: add components header and aside in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import {AppProps} from 'next/app'
+import Head from 'next/head'
 
 import GlobalStyle from 'styles/globals'
+
+import {Header, Aside} from 'components'
 
 export default function MyApp({Component, pageProps}: AppProps): JSX.Element {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Header />
+      <Aside />
       <Component {...pageProps} />
       <GlobalStyle />
     </>
