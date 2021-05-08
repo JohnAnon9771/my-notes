086410ff0fc1870fed7b84285733f9658685b5ab
João Alves <njoao97710@gmail.com>
2021-05-08 00:15:28 +0000

    :heavy_minus_sign: CHORE: remove styled-components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,35 +7,12 @@ import Document, {
   NextScript,
 } from 'next/document'
 
-import {ServerStyleSheet} from 'styled-components'
-
 export default class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: MyApp => props =>
-            sheet.collectStyles(<MyApp {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
-    }
+    const initialProps = await Document.getInitialProps(ctx)
+    return {...initialProps}
   }
 
   render(): JSX.Element {
