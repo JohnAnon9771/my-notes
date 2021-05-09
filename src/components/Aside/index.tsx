6fd0f94ce3bf2085b7e9ab136d66786ae22d0932
João Alves <njoao97710@gmail.com>
2021-05-09 23:13:57 +0000

    :lipstick: STYLE: rename componen nav for aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,11 @@
-import {useRouter} from 'next/dist/client/router'
 import Link from 'next/link'
+import {useRouter} from 'next/router'
 
 import {Setting, Time} from 'assets/icons'
 
 import Select from './Select'
 
-export default function Nav(): JSX.Element {
+export default function Aside(): JSX.Element {
   const router = useRouter()
   const classActive = ['/', '/configuration'].map(route =>
     route === router.route ? 'active' : ''
@@ -13,15 +13,23 @@ export default function Nav(): JSX.Element {
 
   return (
     <aside className="relative w-64 h-full flex flex-col justify-end">
-      <nav className="navigation">
+      <nav className="flex flex-col items-start gap-4 w-full h-5/6 text-2xl">
         <Link href="/">
-          <a className={`navigation-item ${classActive[0] && 'active'}`}>
+          <a
+            className={`navigation-item ${
+              classActive[0] ? 'active' : 'hover:bg-gray-50'
+            }`}
+          >
             <Time viewBox="0 0 24 24" height="32" width="32" />
             Rotina
           </a>
         </Link>
         <Link href="/configuration">
-          <a className={`navigation-item ${classActive[1] && 'active'}`}>
+          <a
+            className={`navigation-item ${
+              classActive[1] ? 'active' : 'hover:bg-gray-50'
+            }`}
+          >
             <Setting viewBox="0 0 24 24" height="32" width="32" />
             Configurações
           </a>
