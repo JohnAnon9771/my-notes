249d5e772b03bdf2f78adb2a9005114ef54a2140
João Alves <njoao97710@gmail.com>
2021-05-10 22:01:19 +0000

    :sparkles: FEAT: add icon when logged

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,18 +7,28 @@ import Sound from './Sound'
 
 export default function Header(): JSX.Element {
   return (
-    <header className="w-screen flex justify-between p-3">
+    <header className="p-5 flex justify-between items-center md:p-3">
+      <button className="lg:hidden shadow-sm h-7 w-7 rounded-md">
+        <span className="h-4 bg-primary w-2" />
+        <span></span>
+      </button>
       <Link href="/">
-        <a className="flex items-center justify-center">
-          <Image src="/logo.svg" width={64} height={64} />
+        <a className="hidden lg:flex items-center justify-center">
+          <div className="">
+            <Image src="/logo.svg" width={64} height={64} />
+          </div>
           <h2 className="text-lg font-black">My Notes</h2>
         </a>
       </Link>
       <div className="flex items-center justify-center gap-2">
-        <Sound className="bg-none border-none" />
-        <a href="https://github.com/JohnAnon9771" about="link for author">
-          <Github with="32" height="32" />
-        </a>
+        <p className="font-bold text-sm text-gray-300 uppercase tracking-widest">
+          JohnAnon9771
+        </p>
+        <img
+          className="rounded-full w-7 h-7 object-cover "
+          src="/images/profile.jpg"
+          alt="profile"
+        />
       </div>
     </header>
   )
