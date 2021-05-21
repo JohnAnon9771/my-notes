438dcd60f4d970ee9b2c75b6e174b6283b046c9c
JohnAnon9771 <njoao97710@gmail.com>
2021-05-21 03:08:32 -0300

    :hammer: FIX: onBlur closing items before change state

--- src/components/Aside/Select/index.tsx
+++ src/components/Aside/Select/index.tsx
@@ -7,32 +7,44 @@ export default function Select(): JSX.Element {
   const [value, setValue] = useState('Português')
 
   return (
-    <div className="flex flex-col gap-3 absolute bottom-3 right-3 w-3/4">
+    <div
+      className="flex flex-col gap-3 absolute bottom-3 right-3 w-3/4"
+      onBlur={e => {
+        if (!e.currentTarget.contains(e.relatedTarget)) {
+          setIsOpen(false)
+        }
+      }}
+    >
       {isOpen && (
-        <div className="border rounded-xl font-semibold p-3 gap-2 shadow-sm z-10">
-          <option
-            className="dropdown-item"
-            onClick={e => setValue(e.currentTarget.innerText)}
-          >
-            Português
-          </option>
-          <option
-            className="dropdown-item"
-            onClick={e => setValue(e.currentTarget.innerText)}
-          >
-            English
-          </option>
+        <div className="border rounded-xl font-semibold p-3 shadow-sm z-10">
+          <ul className="flex flex-col gap-2">
+            <li className="dropdown-item">
+              <button
+                className="focus:outline-none text-left ml-2 h-full w-full font-semibold"
+                onClick={() => setValue('Português')}
+              >
+                Português
+              </button>
+            </li>
+            <li className="dropdown-item">
+              <button
+                className="focus:outline-none text-left ml-2 h-full w-full font-semibold"
+                onClick={() => setValue('English')}
+              >
+                English
+              </button>
+            </li>
+          </ul>
         </div>
       )}
-      <div className="flex border-2 shadow-sm rounded-xl p-3 items-center relative">
-        <span className="absolute p-1 -top-5 left-3 font-semibold text-gray-300 bg-white">
+      <div className="flex border-2 shadow-sm rounded-xl items-center relative">
+        <span className="absolute px-2 -top-5 left-3 font-semibold text-gray-300 bg-white">
           Idioma
         </span>
-        <span>{value}</span>
+        <span className="py-2 px-4">{value}</span>
         <button
-          className="absolute right-3 "
+          className="absolute right-3 focus:outline-none"
           onClick={() => setIsOpen(state => !state)}
-          onBlur={() => setIsOpen(false)}
         >
           <ChevronDown
             className={`transform transition ease-out duration-200 ${
