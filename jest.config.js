bd22d318f29369633ce96a0ff3f55a0c12663c9f
JohnAnon9771 <njoao97710@gmail.com>
2021-06-25 18:23:01 -0300

    :rotating_light: CHORE: add initial config for tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  }
+}
