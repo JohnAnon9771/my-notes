4e7ac2b50682fd662fe1b0c84d0baf0f36a544e0
JohnAnon9771 <njoao97710@gmail.com>
2021-07-05 22:45:33 -0300

    :white_check_mark: TEST: add test for input

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -12,6 +12,14 @@ describe('InputComponent', () => {
     expect(screen.findAllByTestId('input-field')).toBeTruthy()
   })
 
+  it('should attribute value equals value received', () => {
+    const email = 'ndasdas2.com'
+    userEvent.type(screen.getByTestId('input-field'), email)
+    const value = screen.getByTestId('input-field').getAttribute('value')
+
+    expect(value).toBe(email)
+  })
+
   it('should show an error with email incorrect', () => {
     userEvent.type(screen.getByTestId('input-field'), 'ndasdas2.com')
     expect(screen.getByTestId('input-error')).toBeTruthy()
@@ -20,7 +28,14 @@ describe('InputComponent', () => {
   it('should not show an error with email correct', () => {
     const email = 'anything@gmail.com'
     userEvent.type(screen.getByTestId('input-field'), email)
-    // console.log(screen.getAllByTestId('input-error'))
-    // expect(screen.findByTestId('span')).toBe({})
+
+    expect(
+      screen.getByText((content, element) => {
+        return (
+          element.tagName.toLowerCase() === 'span' &&
+          content === 'Valor inválido'
+        )
+      })
+    ).toBeFalsy()
   })
 })
