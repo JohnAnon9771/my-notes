9fbdf3506f38593d3e8d1d34e33603cc350c9670
JohnAnon9771 <njoao97710@gmail.com>
2021-07-07 18:56:34 -0300

    :hammer: CHORE: refact queries in test input

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -11,22 +11,22 @@ describe('InputComponent', () => {
   )
 
   it('should render correctly', () => {
-    expect(screen.findAllByTestId('input-field')).toBeTruthy()
+    expect(screen.getByRole('textbox')).toBeTruthy()
   })
 
   it('should attribute value equals value received', () => {
-    userEvent.type(screen.getByTestId('input-field'), email)
-    expect(screen.getByTestId('input-field').getAttribute('value')).toBe(email)
+    userEvent.type(screen.getByRole('textbox'), email)
+    expect(screen.getByRole('textbox').getAttribute('value')).toBe(email)
   })
 
   it('should show an error with email incorrect', () => {
-    userEvent.type(screen.getByTestId('input-field'), email)
-    expect(screen.getByTestId('input-error').hidden).toBeFalsy()
+    userEvent.type(screen.getByRole('textbox'), email)
+    expect(screen.getByText('Email inválido').hidden).toBeFalsy()
   })
 
   it('should not show an error with email correct', () => {
     const email = 'anything@gmail.com'
-    userEvent.type(screen.getByTestId('input-field'), email)
-    expect(screen.getByTestId('input-error').hidden).toBeTruthy()
+    userEvent.type(screen.getByRole('textbox'), email)
+    expect(screen.getByText('Email inválido').hidden).toBeTruthy()
   })
 })
