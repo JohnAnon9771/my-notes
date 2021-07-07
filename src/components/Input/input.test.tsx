e53d07bb6a81e1e5810a0dd4b5c1c85aabfbcfb0
JohnAnon9771 <njoao97710@gmail.com>
2021-07-07 18:00:08 -0300

    :hammer: CHORE: refact input test

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -4,6 +4,8 @@ import userEvent from '@testing-library/user-event'
 import {Input} from 'components'
 
 describe('InputComponent', () => {
+  const email = 'ndasdas2.com'
+
   beforeEach(() =>
     render(<Input type="email" name="email" id="email" label="Email" />)
   )
@@ -13,22 +15,18 @@ describe('InputComponent', () => {
   })
 
   it('should attribute value equals value received', () => {
-    const email = 'ndasdas2.com'
     userEvent.type(screen.getByTestId('input-field'), email)
-    const value = screen.getByTestId('input-field').getAttribute('value')
-
-    expect(value).toBe(email)
+    expect(screen.getByTestId('input-field').getAttribute('value')).toBe(email)
   })
 
   it('should show an error with email incorrect', () => {
-    userEvent.type(screen.getByTestId('input-field'), 'ndasdas2.com')
+    userEvent.type(screen.getByTestId('input-field'), email)
     expect(screen.getByTestId('input-error').hidden).toBeFalsy()
   })
 
   it('should not show an error with email correct', () => {
     const email = 'anything@gmail.com'
     userEvent.type(screen.getByTestId('input-field'), email)
-
     expect(screen.getByTestId('input-error').hidden).toBeTruthy()
   })
 })
